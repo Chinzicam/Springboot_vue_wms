2ad48e6a0d3d26f774bebdf1dd6b6345d95a6b2f
Chinzicam <a3189797316@163.com>
2023-07-12 20:46:51 +0800

    前端提交

--- SpringBoot_wms/src/main/java/com/czc/controller/UserController.java
+++ SpringBoot_wms/src/main/java/com/czc/controller/UserController.java
@@ -3,10 +3,8 @@ package com.czc.controller;
 import com.czc.entity.User;
 import com.czc.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -23,9 +21,26 @@ import java.util.List;
 public class UserController {
     @Autowired
     private IUserService userService;
+
+    //查询全部
     @GetMapping("/list")
     public List<User> list(){
         userService.list().forEach(System.out::println);
         return userService.list();
+    }
+    //修改
+    @PostMapping("/mod")
+    public boolean mod(@RequestBody User user){
+        return userService.updateById(user);
+    }
+    //新增或修改
+    @PostMapping("/saveOrMod")
+    public boolean saveOrMod(@RequestBody User user){
+        return userService.saveOrUpdate(user);
+    }
+    //删除
+    @GetMapping("/delete")
+    public boolean delete(@RequestParam("id") Integer id){
+        return userService.removeById(id);
     }
 }
